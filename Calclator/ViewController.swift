843ccdc3efa9125a5d0e4698e5fa7db4f0638ec9
mh9898 <mh9898@gmail.com>
2016-04-29 11:09:47 +0300

    firstCommit

--- Calclator/ViewController.swift
+++ Calclator/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var btnSound : AVAudioPlayer!
+    
+    @IBOutlet weak var outputLbl : UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //create a path to the sound file
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let audioURL = NSURL(fileURLWithPath: path!)
+        do{
+            try  btnSound = AVAudioPlayer(contentsOfURL: audioURL)
+        }
+        catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
+        btnSound.prepareToPlay()
+        
+    }
+    
+    @IBAction func numPress (butt: UIButton){
+        btnSound.play()
     }
 
     override func didReceiveMemoryWarning() {
