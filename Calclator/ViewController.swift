a3138009331c8eda8f968c66920f9dd109e06a51
mh9898 <mh9898@gmail.com>
2016-04-30 08:41:27 +0300

    Opertions

--- Calclator/ViewController.swift
+++ Calclator/ViewController.swift
@@ -11,14 +11,23 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    enum Operations : String {
+        case Devide = "/"
+        case Multi = "*"
+        case Plus = "+"
+        case Minus = "-"
+        case Empty = ""
+    }
+    
     var btnSound : AVAudioPlayer!
+    var runningNumber = ""
+    var rightVarString = ""
+    var leftVarString = ""
+    var currentOpertion : Operations = Operations.Empty
+    var result = ""
     
     @IBOutlet weak var outputLbl : UILabel!
-    lkfslkdf;
-    dlfk;dskf;
-    kdljfl
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         //create a path to the sound file
@@ -32,11 +41,83 @@ class ViewController: UIViewController {
         }
         
         btnSound.prepareToPlay()
-        
     }
     
     @IBAction func numPress (butt: UIButton){
-        btnSound.play()
+        playSound()
+        
+        //display the number
+        runningNumber += "\(butt.tag)"
+        outputLbl.text = runningNumber
+    }
+    
+    @IBAction func devideAction(sender: AnyObject) {
+        processOpertion(Operations.Devide)
+    }
+    
+    
+    @IBAction func mutiAction(sender: AnyObject) {
+        processOpertion(Operations.Multi)
+    }
+    
+    
+    @IBAction func minusAction(sender: AnyObject) {
+        processOpertion(Operations.Minus)
+    }
+    
+    
+    @IBAction func plusAction(sender: AnyObject) {
+        processOpertion(.Plus)
+    }
+    
+    
+    @IBAction func equalAction(sender: AnyObject) {
+        processOpertion(currentOpertion)
+        
+}
+    
+    func processOpertion(op: Operations){
+        playSound()
+        
+        if currentOpertion != Operations.Empty {
+            rightVarString = runningNumber
+            outputLbl.text = ""
+            
+            if currentOpertion == Operations.Plus{
+                result = "\(Double(runningNumber)! + Double(rightVarString)!)"
+                            }
+            else if currentOpertion == Operations.Minus{
+                result = "\(Double(runningNumber)! - Double(rightVarString)!)"
+            }
+            else if currentOpertion == Operations.Multi{
+                result = "\(Double(runningNumber)! - Double(rightVarString)!)"
+            }
+            else if currentOpertion == Operations.Devide{
+                result = "\(Double(runningNumber)! - Double(rightVarString)!)"
+            }
+            
+            leftVarString = result
+            outputLbl.text = result
+
+        }
+        else{
+            rightVarString = runningNumber
+            outputLbl.text = ""
+            runningNumber = ""
+            currentOpertion = op
+        }
+        
+        
+    }
+    
+    func playSound(){
+        if btnSound.playing {
+            btnSound.stop()
+        }
+        else{
+            btnSound.play()
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
